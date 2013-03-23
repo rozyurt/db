acae1c884fd27087c11a5439cf01bbe789c9a863
maydogan <meltem.aydogan@bil.omu.edu.tr>
2013-03-23 16:47:28 +0200

    .

--- data/salesman_master.sql
+++ data/salesman_master.sql
@@ -1,6 +1,6 @@
- insert into salesman_master (salesman_no, sal_name, address, city, pincode, state, sal_amt, tgt_to_get, ytd_sales, remarks)
+﻿ insert into salesman_master (salesman_no, sal_name, address, city, pincode, state, sal_amt, tgt_to_get, ytd_sales, remarks)
 values
-    (500001, 'Kiran', 'A/14 worli', 'Bombay', 400002, 'Mah', 3000, 100, 50, 'Google'),
-    (500002, 'Manish', '65,nariman', 'Bombay', 400001, 'Mah', 3000, 200, 100, 'Google'),
-    (500003, 'Ravi', 'P-7 Bandra', 'Bombay', 400032, 'Mah', 3000, 200, 100, 'Google'),
-    (500004, 'Ashish', 'A/5 Juhu', 'Bombay', 400044, 'Mah', 3500, 200, 150, 'Google');
+    ('S00001', 'Kiran', 'A/14 worli', 'Bombay', 400002, 'Mah', 3000, 100, 50, 'Google'),
+    ('S00002', 'Manish', '65,nariman', 'Bombay', 400001, 'Mah', 3000, 200, 100, 'Google'),
+    ('S00003', 'Ravi', 'P-7 Bandra', 'Bombay', 400032, 'Mah', 3000, 200, 100, 'Google'),
+    ('S00004', 'Ashish', 'A/5 Juhu', 'Bombay', 400044, 'Mah', 3500, 200, 150, 'Google');
--- table/salesman_master.sql
+++ table/salesman_master.sql
@@ -9,6 +9,6 @@ sal_amt numeric(8,2) NOT NULL ,
 tgt_to_get numeric(6,2) NOT NULL ,
 ytd_sales numeric(6,2) NOT NULL ,
 remarks varchar (30),
-check (salesman_no like 's%'),
+check (salesman_no like 'S%'),
 check (sal_amt != 0 and tgt_to_get != 0 and ytd_sales !=0)
 );
